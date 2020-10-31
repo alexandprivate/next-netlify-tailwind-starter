a92e8f91160af8a1469b30ca90b2885304f2e698
alexandprivate <alexandprivate@gmail.com>
2020-10-31 14:58:33 -0500

    version update

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
-import Footer from "./footer";
-import Header from "./header";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Layout({ children }) {
     return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../tailwind.css";
-import Layout from "components/layout";
+import Layout from "components/layout/Layout";
 
 function App({ Component, pageProps }) {
     return (
