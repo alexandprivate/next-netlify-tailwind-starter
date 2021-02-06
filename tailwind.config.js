5f7e2a3190f13f5854bf9f443c37e2638047aca9
alexandprivate <alexandprivate@gmail.com>
2021-02-05 21:29:44 -0500

    tailwind update
    tailwindconfig update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    darkMode: "class",
+    i18n: {
+        locales: ["en-US"],
+        defaultLocale: "en-US",
+    },
     purge: [
         "./components/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,6 +11,16 @@ module.exports = {
     theme: {
         extend: {},
     },
-    variants: {},
+    variants: {
+        extend: {
+            backgroundColor: ["checked"],
+            borderColor: ["checked"],
+            inset: ["checked"],
+            zIndex: ["hover", "active"],
+        },
+    },
     plugins: [],
+    future: {
+        purgeLayersByDefault: true,
+    },
 };
